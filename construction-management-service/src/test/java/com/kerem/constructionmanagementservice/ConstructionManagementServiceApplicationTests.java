71435a43ca3fc08a1e89f495318c2519792f53b4
Kerem <kmsriv.185@gmail.com>
2025-02-15 19:15:41 +0300

    Initialize construction management service with Maven setup and basic application structure

---
+++ construction-management-service/src/test/java/com/kerem/constructionmanagementservice/ConstructionManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kerem.constructionmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstructionManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
